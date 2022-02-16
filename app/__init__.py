b0ef82d2d1e7a5630416f80fcd624b4fe60b3c9c
Am <amxelemara@Gmail.com>
2022-02-16 10:47:56 +0000

    Move app definition to __init__ file

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+DBUSER = 'postgres'
+DBPASS = 'postgres'
+DBHOST = 'localhost'
+DBPORT = '5431'
+DBNAME = 'postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql+psycopg1://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=DBUSER,
+        passwd=DBPASS,
+        host=DBHOST,
+        port=DBPORT,
+        db=DBNAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "secret"
