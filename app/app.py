4c79d6cc7b87d5a4a8ca056a77b853857154e632
Am <amxelemara@Gmail.com>
2022-02-16 10:49:15 +0000

    Import app from init file
    Also run db.create_all when this file is run

--- app/app.py
+++ app/app.py
@@ -2,17 +2,7 @@ import time
 from flask import Flask, render_template, flash, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from db import db
-from setup import app
-
-app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-
-
-def database_initialization_sequence():
-    db.create_all()
-
-    db.session.rollback()
-    db.session.commit()
+from app import app
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,13 +11,5 @@ def home():
 
 
 if __name__ == '__main__':
-    dbstatus = False
-    while dbstatus == False:
-        try:
-            db.create_all()
-        except:
-            time.sleep(2)
-        else:
-            dbstatus = True
-    database_initialization_sequence()
+    db.create_all()
     app.run(debug=True, host='0.0.0.0')
